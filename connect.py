25958e06851aa079a47dbbc47805a4b56cd2f2a4
artandopel <62048301+artandopel@users.noreply.github.com>
2020-03-11 15:44:08 +0700

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,10 +1,10 @@
 import getpass
-import telnetlib
+import telnetlib        
 
 
-HOST = input("ip: ")
-user = input("Enter your remote account: ")
-password = getpass.getpass()
+HOST = input("ip: ")     # ip interface on router
+user = input("Enter your remote account: ")  # username on router
+password = getpass.getpass()                 # password on router 
 
 tn = telnetlib.Telnet(HOST)
 
@@ -17,19 +17,19 @@ if password:
 
 
 tn.write(b"enable\n")
-tn.write(b"1234\n")
+tn.write(b"1234\n")         #password after enable  
 tn.write(b"config ter\n")
 
-tn.write(b"interface loopback 1 \n")
-tn.write(b"ip add 10.10.10.10 255.255.255.255\n")
+tn.write(b"interface loopback 1 \n")              #ip interface loopback 1 
+tn.write(b"ip add 10.10.10.10 255.255.255.255\n") #set ip address in loopback 1
 
 print("DONE")
-tn.write(b"interface loopback 2 \n")
-tn.write(b" ip add 11.11.11.11 255.255.255.255\n")
+tn.write(b"interface loopback 2 \n")              #ip interface loopback 2
+tn.write(b" ip add 11.11.11.11 255.255.255.255\n") #set ip address in loopback 2
 
 print("DONE")
-tn.write(b"interface loopback 3 \n")
-tn.write(b" ip add 12.12.12.12 255.255.255.255\n")
+tn.write(b"interface loopback 3 \n")              #ip interface loopback 3
+tn.write(b" ip add 12.12.12.12 255.255.255.255\n") #set interface loopback 3
 
 print("DONE")
 print(tn.read_all().decode('ascii'))
